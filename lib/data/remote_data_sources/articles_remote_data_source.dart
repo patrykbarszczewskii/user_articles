556ea133b57bc1f3e725a8ac09075f88a8dd200f
Patryk Barszczewski <patryk.barszczewskii@gmail.com>
2023-01-03 11:05:17 +0100

    Implement injectable

--- lib/data/remote_data_sources/articles_remote_data_source.dart
+++ lib/data/remote_data_sources/articles_remote_data_source.dart
@@ -1,13 +1,17 @@
 import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:user_articles/domain/models/article_model.dart';
 
 part 'articles_remote_data_source.g.dart';
 
-@RestApi(baseUrl: 'https://my-json-server.typicode.com/adamsmaka/json-demo')
+@RestApi()
+@injectable
 abstract class ArticlesRemoteRetrofitDataSource {
-  factory ArticlesRemoteRetrofitDataSource(Dio dio, {String baseUrl}) =
-      _ArticlesRemoteRetrofitDataSource;
+  @factoryMethod
+  factory ArticlesRemoteRetrofitDataSource(
+    Dio dio,
+  ) = _ArticlesRemoteRetrofitDataSource;
 
   @GET("/articles")
   Future<List<ArticleModel>> getArticles();
--- lib/data/remote_data_sources/authors_remote_data_source.dart
+++ lib/data/remote_data_sources/authors_remote_data_source.dart
@@ -1,26 +1,20 @@
 import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:user_articles/domain/models/author_model.dart';
 
 part 'authors_remote_data_source.g.dart';
 
-@RestApi(baseUrl: 'https://my-json-server.typicode.com/adamsmaka/json-demo')
+@RestApi()
+@injectable
 abstract class AuthorsRemoteRetrofitDataSource {
-  factory AuthorsRemoteRetrofitDataSource(Dio dio, {String baseUrl}) =
-      _AuthorsRemoteRetrofitDataSource;
+  @factoryMethod
+  factory AuthorsRemoteRetrofitDataSource(
+    Dio dio,
+  ) = _AuthorsRemoteRetrofitDataSource;
 
   @GET("/users")
   Future<List<AuthorModel>> getAuthors();
 }
 
-// class AuthorsRemoteDioDataSource {
-//   Future<List<Map<String, dynamic>>?> getAuthors() async {
-//     final response = await Dio().get<List<dynamic>>(
-//         'https://my-json-server.typicode.com/adamsmaka/json-demo/users');
-//     final listDynamic = response.data;
-//     if (listDynamic == null) {
-//       return null;
-//     }
-//     return listDynamic.map((e) => e as Map<String, dynamic>).toList();
-//   }
-// }
+
